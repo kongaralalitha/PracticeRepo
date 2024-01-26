c5801c003b309b3d45fe4429441656163dba7c34
kongaralalitha <kongaralalitha@gmail.com>
2024-01-26 13:35:45 -0600

    Update RecursionFunction.java
    added class methods

--- interviewExercises/RecursionFunction.java
+++ interviewExercises/RecursionFunction.java
@@ -1,5 +1,26 @@
 package interviewExercises;
 
 public class RecursionFunction {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int base=sc.nextInt();
+		int exponent=sc.nextInt();
+		
+		int res=recursiveFunction(base,exponent);
+		System.out.println(res);
+	
+		
+
+	}
+	public static int recursiveFunction(int b,int e)
+	{
+		if(e==0)
+			{
+			return 1;
+			}
+		return b*recursiveFunction(b,(e-1));
+	}
+	
+
 
 }
